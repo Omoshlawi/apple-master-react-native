03161b38eca65d5f34773fa298c3e45b02aeba69
Laurent Ouma <lawiomosh3@gmail.com>
2023-03-28 19:44:15 +0300

    Drop down working good but not developer friendly, form state working greate

--- app/screens/user/ProfileScreen.js
+++ app/screens/user/ProfileScreen.js
@@ -10,6 +10,7 @@ import {
 } from "react-native-paper";
 import colors from "../../utils/colors";
 import TextInputField from "../../components/input/TextInputField";
+import DropDown from "../../components/input/DropDown";
 
 const ProfileScreen = ({ navigation, route }) => {
   const {
@@ -20,6 +21,18 @@ const ProfileScreen = ({ navigation, route }) => {
     username,
   } = route.params;
   const [edit, setEdit] = useState(true);
+  const [genders, setGenders] = useState([
+    { label: "Male", value: "male" },
+    { label: "Female", value: "female" },
+    { label: "Other", value: "other" },
+  ]);
+  const [formState, setFormState] = useState({
+    first_name,
+    last_name,
+    gender,
+    phone_number,
+    image,
+  });
   return (
     <View>
       {!edit && (
@@ -76,16 +89,55 @@ const ProfileScreen = ({ navigation, route }) => {
 
       {edit && (
         <View style={styles.card} elevation={0}>
-          <TextInputField placeholder="First name" icon="account-edit" />
-          <TextInputField placeholder="Last Name" icon="account-edit" />
-          <TextInputField placeholder="Gender" icon="human-edit" />
-          <TextInputField placeholder="Phone Number" icon="phone" />
+          <TextInputField
+            placeholder="First name"
+            icon="account-edit"
+            value={formState.first_name}
+            onChangeText={(first_name) =>
+              setFormState({ ...formState, first_name })
+            }
+          />
+          <TextInputField
+            placeholder="Last Name"
+            icon="account-edit"
+            value={formState.last_name}
+            onChangeText={(last_name) =>
+              setFormState({ ...formState, last_name })
+            }
+          />
+          <DropDown
+            placeholder="Gender"
+            icon="human-edit"
+            // value is a string and not object
+            value={formState.gender}
+            // parameter is a fn that returns value, not object
+            setValue={(value) =>
+              setFormState({ ...formState, gender: value() })
+            }
+            data={genders}
+            setData={setGenders}
+          />
+          <TextInputField
+            placeholder="Phone Number"
+            icon="phone"
+            value={formState.phone_number}
+            onChangeText={(phone_number) =>
+              setFormState({ ...formState, phone_number })
+            }
+          />
         </View>
       )}
 
       <Card style={styles.card} elevation={1}>
         <Card.Content>
-          <Button dark mode="outlined" icon="pen">
+          <Button
+            dark
+            mode="outlined"
+            icon="pen"
+            onPress={() => {
+              console.log(formState);
+            }}
+          >
             Edit Profile
           </Button>
         </Card.Content>
