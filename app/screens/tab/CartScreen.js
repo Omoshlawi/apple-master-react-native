1c3ea24bb06238970d946300375ba9acd56ad48e
Laurent Ouma <lawiomosh3@gmail.com>
2023-04-05 18:56:52 +0300

    Done with cart screen, all working good, hiding fab on empty cart

--- app/screens/tab/CartScreen.js
+++ app/screens/tab/CartScreen.js
@@ -18,6 +18,7 @@ import RatingBar from "../../components/ratingbar/RatingBar";
 import QuanterSizer from "../../components/input/QuanterSizer";
 import routes from "../../navigation/routes";
 import { useShop } from "../../api/hooks";
+import Logo from "../../components/Logo";
 
 const CartScreen = ({ navigation }) => {
   const {
@@ -51,115 +52,128 @@ const CartScreen = ({ navigation }) => {
   const { open } = state;
   return (
     <AppSafeArea>
-      <FlatList
-        data={cartItems}
-        keyExtractor={({ product: { url } }) => url}
-        renderItem={({ item }) => {
-          const {
-            quantity,
-            product: {
-              name,
-              image,
-              description,
-              price,
-              rating,
-              tags,
-              images,
-              updated,
-              category: { name: category },
-              reviews: { count: reviews },
-            },
-          } = item;
-          // console.log(item);
-          return (
-            <List.Item
-              style={styles.item}
-              title={() => (
-                <Text variant="titleLarge" style={{ fontWeight: "bold" }}>
-                  {name}
-                </Text>
-              )}
-              description={() => (
-                <List.Item
-                  title={() => <Text>{`${category} | ${price}`}</Text>}
-                  description={() => (
-                    <Text variant="bodyLarge" style={{ fontWeight: "bold" }}>
-                      Ksh. {parseFloat(price) * parseFloat(quantity)}
-                    </Text>
-                  )}
-                />
-              )}
-              left={(props) => (
-                <Avatar.Image {...props} source={{ uri: image }} size={70} />
-              )}
-              right={(props) => (
-                <Card.Actions
-                  style={{
-                    flexDirection: "column",
-                    padding: 0,
-                    alignItems: "flex-end",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <QuanterSizer
-                    value={quantity}
-                    onIncrement={() => {
-                      addToCart({ ...item, quantity: quantity + 1 });
-                    }}
-                    onDecrement={() => {
-                      if (quantity > 1)
-                        addToCart({ ...item, quantity: quantity - 1 });
-                    }}
+      {cartItems.length > 0 ? (
+        <FlatList
+          data={cartItems}
+          keyExtractor={({ product: { url } }) => url}
+          renderItem={({ item }) => {
+            const {
+              quantity,
+              product: {
+                name,
+                image,
+                description,
+                price,
+                rating,
+                tags,
+                images,
+                updated,
+                category: { name: category },
+                reviews: { count: reviews },
+              },
+            } = item;
+            // console.log(item);
+            return (
+              <List.Item
+                style={styles.item}
+                title={() => (
+                  <Text variant="titleLarge" style={{ fontWeight: "bold" }}>
+                    {name}
+                  </Text>
+                )}
+                description={() => (
+                  <List.Item
+                    title={() => <Text>{`${category} | ${price}`}</Text>}
+                    description={() => (
+                      <Text variant="bodyLarge" style={{ fontWeight: "bold" }}>
+                        Ksh. {parseFloat(price) * parseFloat(quantity)}
+                      </Text>
+                    )}
                   />
-                  <IconButton
-                    {...props}
-                    icon="delete"
-                    style={{ marginRight: 0 }}
-                    iconColor={colors.danger}
-                    onPress={() => {
-                      deleteFromCart(item.product);
+                )}
+                left={(props) => (
+                  <Avatar.Image {...props} source={{ uri: image }} size={70} />
+                )}
+                right={(props) => (
+                  <Card.Actions
+                    style={{
+                      flexDirection: "column",
+                      padding: 0,
+                      alignItems: "flex-end",
+                      justifyContent: "space-between",
                     }}
-                  />
-                </Card.Actions>
-              )}
+                  >
+                    <QuanterSizer
+                      value={quantity}
+                      onIncrement={() => {
+                        addToCart({ ...item, quantity: quantity + 1 });
+                      }}
+                      onDecrement={() => {
+                        if (quantity > 1)
+                          addToCart({ ...item, quantity: quantity - 1 });
+                      }}
+                    />
+                    <IconButton
+                      {...props}
+                      icon="delete"
+                      style={{ marginRight: 0 }}
+                      iconColor={colors.danger}
+                      onPress={() => {
+                        deleteFromCart(item.product);
+                      }}
+                    />
+                  </Card.Actions>
+                )}
+              />
+            );
+          }}
+        />
+      ) : (
+        <View style={styles.emptyContainer}>
+          <View>
+            <Logo variant="black" />
+          </View>
+          <Text variant="headlineMedium" style={{ padding: 20 }}>
+            There are not items in your cart yet
+          </Text>
+        </View>
+      )}
+      {cartItems.length > 0 && (
+        <Provider>
+          <Portal>
+            <FAB.Group
+              open={open}
+              fabStyle={styles.fab}
+              label={`Ksh. ${totalCost}`}
+              backdropColor={colors.transparent}
+              visible
+              icon={open ? "close" : "dots-vertical"}
+              actions={[
+                {
+                  icon: "plus",
+                  onPress: () => navigation.navigate(routes.HOME_SCREEN),
+                },
+                {
+                  icon: "notification-clear-all",
+                  label: "Clear",
+                  onPress: clearAll,
+                },
+                {
+                  icon: "share-all",
+                  label: "Order now",
+                  onPress: handleAddOrder,
+                },
+              ]}
+              onStateChange={onStateChange}
+              onPress={() => {
+                if (open) {
+                  // do something if the speed dial is open
+                }
+              }}
             />
-          );
-        }}
-      />
-      <Provider>
-        <Portal>
-          <FAB.Group
-            open={open}
-            fabStyle={styles.fab}
-            label={`Ksh. ${totalCost}`}
-            backdropColor={colors.transparent}
-            visible
-            icon={open ? "close" : "dots-vertical"}
-            actions={[
-              {
-                icon: "plus",
-                onPress: () => navigation.navigate(routes.HOME_SCREEN),
-              },
-              {
-                icon: "notification-clear-all",
-                label: "Clear",
-                onPress: clearAll,
-              },
-              {
-                icon: "share-all",
-                label: "Order now",
-                onPress: handleAddOrder,
-              },
-            ]}
-            onStateChange={onStateChange}
-            onPress={() => {
-              if (open) {
-                // do something if the speed dial is open
-              }
-            }}
-          />
-        </Portal>
-      </Provider>
+          </Portal>
+        </Provider>
+      )}
       <Snackbar
         visible={visible}
         onDismiss={onDismissSnackBar}
@@ -194,5 +208,11 @@ const styles = StyleSheet.create({
     position: "absolute",
     right: 0,
     bottom: 0,
+  },
+  emptyContainer: {
+    justifyContent: "center",
+    flex: 1,
+    alignItems: "center",
+    opacity: 0.4,
   },
 });
